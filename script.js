6a870c6d35f121bbc26fd460d4a55677eb613aad
Jeffery Lin <jeffery.lin@student.tdsb.on.ca>
2025-03-01 11:59:52 -0500

    Changed hero section image. Added 2 images for the hero section to cycle through using javascript.

--- script.js
+++ script.js
@@ -15,4 +15,21 @@ featuredRentButtons.forEach((button) => {
         const movieTitle = button.parentElement.querySelector('.featuredTitle').textContent;
         alert(`Rented: ${movieTitle}`);
     });
-}); 
+});
+
+const heroImages = ['hero1.jpg', 'hero2.jpg'];
+const heroImage = document.querySelector('.heroImage');
+let currentIndex = 0;
+
+setInterval(() => {
+    heroImage.style.opacity = 0;
+    setTimeout(() => {
+      currentIndex = (currentIndex + 1) % heroImages.length;
+      heroImage.src = heroImages[currentIndex];
+      heroImage.addEventListener('load', function onLoad() {
+            heroImage.style.opacity = 1;
+            heroImage.removeEventListener('load', onLoad);
+      });
+      
+    }, 800); 
+}, 5000);
